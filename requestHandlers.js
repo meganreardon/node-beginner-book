62d5c0da1f6c2101f4ce2519ef6ffdf6e25c3a71
Megan Reardon <meganr@gmail.com>
2016-12-30 15:16:32 -0800

    post requests added

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,38 +1,76 @@
 'use strict';
 
 // ----------------
-// #4: Non-blocking
+// #5: POST request
 // ----------------
 
-var exec = require('child_process').exec;
+var querystring = require('querystring');
 
 function start(res) {
   console.log('Request handler "start" was called.');
 
-  // replaced end of non-blocking example
-  // exec('ls -lah', function (error, stdout, stderr) {
-  //   res.writeHead(200, {'Content-Type': 'text/plain'});
-  //   res.write(stdout);
-  //   res.end();
-  // });
+  var body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" content="text/html; '+
+    'charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/upload" method="post">'+
+    '<textarea name="text" rows="20" cols="60"></textarea>'+
+    '<input type="submit" value="Submit text" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
 
-  exec('find /',
-    { timeout: 10000, maxBuffer: 20000*1024 }, function (error, stdout, stderr) {
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.write(stdout);
-      res.end();
-    });
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(body);
+  res.end();
 }
 
-function upload(res) {
+function upload(res, postData) {
   console.log('Request handler "upload" was called.');
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('Hello Upload');
+  res.write('You\'ve send the text: ' +
+  querystring.parse(postData).text);
   res.end();
 }
 
 exports.start = start;
 exports.upload = upload;
+
+// ----------------
+// #4: Non-blocking
+// ----------------
+
+// var exec = require('child_process').exec;
+//
+// function start(res) {
+//   console.log('Request handler "start" was called.');
+//
+//   // replaced end of non-blocking example
+//   // exec('ls -lah', function (error, stdout, stderr) {
+//   //   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   //   res.write(stdout);
+//   //   res.end();
+//   // });
+//
+//   exec('find /',
+//     { timeout: 10000, maxBuffer: 20000*1024 }, function (error, stdout, stderr) {
+//       res.writeHead(200, {'Content-Type': 'text/plain'});
+//       res.write(stdout);
+//       res.end();
+//     });
+// }
+//
+// function upload(res) {
+//   console.log('Request handler "upload" was called.');
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.write('Hello Upload');
+//   res.end();
+// }
+//
+// exports.start = start;
+// exports.upload = upload;
 
 // -------------------------
 // #3: handlers and response
