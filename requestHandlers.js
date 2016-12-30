063e0242577e0a8210cd3f0f71e78f86a8991a06
Megan Reardon <meganr@gmail.com>
2016-12-30 14:55:08 -0800

    non blocking example

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,25 +1,63 @@
 'use strict';
 
+// ----------------
+// #4: Non-blocking
+// ----------------
+
 var exec = require('child_process').exec;
 
-function start() {
+function start(res) {
   console.log('Request handler "start" was called.');
-  var content = 'empty';
 
-  exec('ls -lah', function (error, stdout, stderr) {
-    content = stdout;
-  });
+  // replaced end of non-blocking example
+  // exec('ls -lah', function (error, stdout, stderr) {
+  //   res.writeHead(200, {'Content-Type': 'text/plain'});
+  //   res.write(stdout);
+  //   res.end();
+  // });
 
-  return content;
+  exec('find /',
+    { timeout: 10000, maxBuffer: 20000*1024 }, function (error, stdout, stderr) {
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.write(stdout);
+      res.end();
+    });
 }
 
-function upload() {
+function upload(res) {
   console.log('Request handler "upload" was called.');
-  return 'Hello Upload.';
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('Hello Upload');
+  res.end();
 }
 
 exports.start = start;
 exports.upload = upload;
+
+// -------------------------
+// #3: handlers and response
+// -------------------------
+//
+// var exec = require('child_process').exec;
+//
+// function start() {
+//   console.log('Request handler "start" was called.');
+//   var content = 'empty';
+//
+//   exec('ls -lah', function (error, stdout, stderr) {
+//     content = stdout;
+//   });
+//
+//   return content;
+// }
+//
+// function upload() {
+//   console.log('Request handler "upload" was called.');
+//   return 'Hello Upload.';
+// }
+//
+// exports.start = start;
+// exports.upload = upload;
 
 // --------------------
 // #2: blocking example
--- router.js
+++ router.js
@@ -1,17 +1,39 @@
 'use strict';
 
-function route(handle, pathname) {
-  console.log('About to route a request for ', pathname);
+// ---------------
+// #2 Non-blocking
+// ---------------
+
+function route(handle, pathname, res) {
+  console.log('About to route a request for', pathname);
   if (typeof handle[pathname] === 'function') {
-    handle[pathname]();
+    handle[pathname](res);
   } else {
-    console.log('No request handler found for ', pathname);
+    console.log('No request handler fournd for ', pathname);
+    res.writeHead(404, {'Content-Type': 'text/plain'});
+    res.write('404 Not Found');
+    res.end();
   }
 }
+
+exports.route = route;
+
+// ------------------------
+// #1 Routing and responses
+// ------------------------
+
+// function route(handle, pathname) {
+//   console.log('About to route a request for ', pathname);
+//   if (typeof handle[pathname] === 'function') {
+//     handle[pathname]();
+//   } else {
+//     console.log('No request handler found for ', pathname);
+//   }
+// }
 
 // was orig
 // function route(pathname) {
 //   console.log('About to route a request for ', pathname);
 // }
 
-exports.route = route;
+// exports.route = route;
