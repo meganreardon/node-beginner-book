2d98116662e12286d8da6dd0eb0669a4cab81b49
Megan Reardon <meganr@gmail.com>
2016-12-30 14:33:11 -0800

    request handlers reponse

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,12 +1,61 @@
 'use strict';
 
+var exec = require('child_process').exec;
+
 function start() {
   console.log('Request handler "start" was called.');
+  var content = 'empty';
+
+  exec('ls -lah', function (error, stdout, stderr) {
+    content = stdout;
+  });
+
+  return content;
 }
 
 function upload() {
   console.log('Request handler "upload" was called.');
+  return 'Hello Upload.';
 }
 
 exports.start = start;
 exports.upload = upload;
+
+// --------------------
+// #2: blocking example
+// --------------------
+//
+// function start() {
+//   console.log('Request handler "start" was called');
+//
+//   function sleep(milliSeconds) {
+//     var startTime = new Date().getTime();
+//     while (new Date().getTime() < startTime + milliSeconds);
+//   }
+//
+//   sleep(10000);
+//   return 'Hello Start.';
+// }
+//
+// function upload() {
+//   console.log('Request handler "upload" was called.');
+//   return 'Hello Upload';
+// }
+//
+// exports.start = start;
+// exports.upload = upload;
+
+// ------------------
+// #1: basic handling
+// ------------------
+//
+// function start() {
+//   console.log('Request handler "start" was called.');
+// }
+//
+// function upload() {
+//   console.log('Request handler "upload" was called.');
+// }
+//
+// exports.start = start;
+// exports.upload = upload;
