844135ab56ce71f4dbb8c8021f649b76836f27f9
Megan Reardon <meganr@gmail.com>
2016-12-30 15:40:17 -0800

    half way through image upload code

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,12 +1,13 @@
 'use strict';
 
-// ----------------
-// #5: POST request
-// ----------------
+// -----------
+// #6: uploads
+// -----------
 
-var querystring = require('querystring');
+var querystring = require('querystring'),
+  fs = require('fs');
 
-function start(res) {
+function start(res, postData) {
   console.log('Request handler "start" was called.');
 
   var body = '<html>'+
@@ -35,8 +36,53 @@ function upload(res, postData) {
   res.end();
 }
 
+function show(res) {
+  console.log('Request handler "show" was called.');
+  res.writeHead(200, {'Content-Type': 'image/png'});
+  fs.createReadStream('/tmp/test.png').pipe(res);
+}
+
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
+
+// ----------------
+// #5: POST request
+// ----------------
+
+// var querystring = require('querystring');
+//
+// function start(res) {
+//   console.log('Request handler "start" was called.');
+//
+//   var body = '<html>'+
+//     '<head>'+
+//     '<meta http-equiv="Content-Type" content="text/html; '+
+//     'charset=UTF-8" />'+
+//     '</head>'+
+//     '<body>'+
+//     '<form action="/upload" method="post">'+
+//     '<textarea name="text" rows="20" cols="60"></textarea>'+
+//     '<input type="submit" value="Submit text" />'+
+//     '</form>'+
+//     '</body>'+
+//     '</html>';
+//
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   res.write(body);
+//   res.end();
+// }
+//
+// function upload(res, postData) {
+//   console.log('Request handler "upload" was called.');
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.write('You\'ve send the text: ' +
+//   querystring.parse(postData).text);
+//   res.end();
+// }
+//
+// exports.start = start;
+// exports.upload = upload;
 
 // ----------------
 // #4: Non-blocking
--- server.js
+++ server.js
@@ -6,6 +6,7 @@
 
 var http = require('http');
 var url = require('url');
+var formidable = require('formidable');
 
 function start(route, handle) {
   function onRequest(req, res) {
