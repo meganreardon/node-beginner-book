351f76cb005efe95bbd749646b339092e122198f
Megan Reardon <meganr@gmail.com>
2016-12-30 14:13:45 -0800

    basic routes done

--- index.js
+++ index.js
@@ -2,5 +2,11 @@
 
 var server = require('./server.js');
 var router = require('./router.js');
+var requestHandlers = require('./requestHandlers');
 
-server.start(router.route);
+var handle = {};
+handle['/'] = requestHandlers.start;
+handle['/start'] = requestHandlers.start;
+handle['/upload'] = requestHandlers.upload;
+
+server.start(router.route, handle);
--- router.js
+++ router.js
@@ -1,7 +1,27 @@
 'use strict';
 
-function route(pathname) {
+function route(handle, pathname) {
   console.log('About to route a request for ', pathname);
+  if (typeof handle[pathname] === 'function') {
+    handle[pathname]();
+  } else {
+    console.log('No request handler found for ', pathname);
+  }
+}
+
+// was orig
+// function route(pathname) {
+//   console.log('About to route a request for ', pathname);
+// }
+
+function start() {
+  console.log('Request handler "start" was called.');
+}
+
+function upload() {
+  console.log('Request handler "upload" was called.');
 }
 
 exports.route = route;
+exports.start = start;
+exports.upload = upload;
